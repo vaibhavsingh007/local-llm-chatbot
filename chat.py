640defb3efccc4dcc5a7a6d184479a85da9de7fe
Vaibhav Singh <vsingh.uic@gmail.com>
2024-10-08 12:11:32 +0000

    Implemnted model switch and handling of special user instructions

--- chat.py
+++ chat.py
@@ -33,48 +33,63 @@ def get_prompt(instruction: str, history: List[str] = None) -> str:
         if len(history) > 0:
             prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
 
-        prompt += f"{instruction}\n\n{instruction} [/INST]"
+        prompt += f"\n\n{instruction} [/INST]"
+    print(prompt)
     return prompt
 
 
-def handle_special_message(msg: str) -> None:
+def handle_special_message(msg: str) -> str | None:
     global model_in_use
 
     if msg == "use llama2":
         model_in_use = 'llama2'
+        set_llm()
+        return "Model changed to Llama"
     elif msg == "use orca":
         model_in_use = "orca"
+        set_llm()
+        return "Model changed to Orca"
+    elif msg == "which model":
+        return model_in_use
+    elif msg == "history":
+        return cl.user_session.get("message_history")
+    elif msg == "forget everything":
+        cl.user_session.set("message_history", [])
+        return "Uh oh, I've just forgotten our conversation history"
     else:
-        pass
-
-    set_llm()
+        return None
 
 
 # Global model setter
 def set_llm() -> None:
     global llm
     llm = AutoModelForCausalLM.from_pretrained(
-        model_path_or_repo_id=llm_config[model_in_use].path,
-        model_file=llm_config[model_in_use].file)
+        model_path_or_repo_id=llm_config[model_in_use]['path'],
+        model_file=llm_config[model_in_use]['file'])
 
 
 @cl.on_chat_start
 def on_chat_start():
     cl.user_session.set("message_history", [])
-    set_llm('orca')
+    set_llm()
 
 
 @ cl.on_message
 async def on_message(message: cl.Message):
-    handle_special_message(message.content)
-    history = cl.user_session.get("message_history")
-    prompt = get_prompt(message.content, history)
-    msg = cl.Message(content="")
-    await msg.send()
+    handler_response = handle_special_message(message.content)
 
-    response = ""
-    for word in llm(prompt, stream=True):
-        await msg.stream_token(word)
-        response += word
-    await msg.update()
-    history.append(response)
+    if handler_response is not None:
+        msg = cl.Message(content=handler_response)
+        await msg.send()
+    else:
+        history = cl.user_session.get("message_history")
+        prompt = get_prompt(message.content, history)
+        msg = cl.Message(content="")
+        await msg.send()
+
+        response = ""
+        for word in llm(prompt, stream=True):
+            await msg.stream_token(word)
+            response += word
+        await msg.update()
+        history.append(response)
