6aa658b9ff34effba259d58c6e0ddef96e3f26f8
Vaibhav Singh <vsingh.uic@gmail.com>
2024-10-09 15:57:31 +0000

    Implemented conversation history in Langchain

--- chat_langchain.py
+++ chat_langchain.py
@@ -1,6 +1,7 @@
 from langchain_community.llms.ctransformers import CTransformers
 from langchain_core.prompts import PromptTemplate
 from langchain.chains.llm import LLMChain
+from langchain.memory.buffer import ConversationBufferMemory
 
 llm = CTransformers(
     model="zoltanctoth/orca_mini_3B-GGUF",
@@ -10,7 +11,10 @@ llm = CTransformers(
 )
 
 prompt_template = """### System:\nYou are an AI assistant that gives short and concise answers.
+            Take this context into account when answering the question: {context}
             \n\n### User:\n{instruction}\n\n### Response:\n"""
 prompt = PromptTemplate(template=prompt_template, input_variables=["instruction"])
-chain = LLMChain(llm=llm, prompt=prompt)
+memory = ConversationBufferMemory(memory_key="context")
+chain = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=True)
 print(chain.invoke({"instruction": "What is the capital of India?"}))
+print(chain.invoke({"instruction": "And which one is of the US?"}))
